d585c8523a8ece3466466d172e3e24030684580d
Anjali <anjalisharmaaa656@gmail.com>
2024-11-02 14:25:41 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-
+import ReactDOM from "react-dom/client"; // This is the correct import for React 18
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
