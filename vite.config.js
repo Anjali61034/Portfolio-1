b2d9a1b27427778a88436ae48bf9f04b0701190c
Anjali61034 <153043480+Anjali61034@users.noreply.github.com>
2024-11-02 14:39:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Only if necessary for subdirectory hosting
+  base: '/', // Only if necessary for subdirectory hosting
 });
 
